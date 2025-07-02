a00425b34e8e0bf9ef0de9954becc0a2c6445aff
logesh000777r <logesh000777r@gmail.com>
2025-07-02 14:28:31 +0530

    Greetings

--- src/com/vetias/java/basics/Greetings.java
+++ src/com/vetias/java/basics/Greetings.java
@@ -1,6 +1,6 @@
 package com.vetias.java.basics;
 
-public class Greeings {
+public class Greetings {
     public static void main(String[] args){
         System.out.println("My First java Program!");
     }
