b6d0fada4a260b7497fc38c1d474edba4e82187c
logesh000777r <logesh000777r@gmail.com>
2025-07-07 15:51:00 +0530

    end

---
+++ src/com/vetias/java/workshop/basics/NameGenerator.java
@@ -0,0 +1,30 @@
+package com.vetias.java.workshop.basics;
+import java.util.Scanner;
+
+public class NameGenerator {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = input.nextLine();
+        System.out.println("Enter your Gender(Male/Female):");
+        String gender =input.nextLine();
+        System.out.println("Enter yourqualification:");
+        String qualification =input.nextLine();
+        StringBuilder ob = new StringBuilder();
+        if("male".equalsIgnoreCase(gender)) {
+            ob.append("Mr. ");
+            ob.append(name).append(" ").append(qualification);
+            String result = ob.toString();
+            System.out.println(result);
+        } else {
+            ob.append("Ms. ");
+            ob.append(name).append(" ").append(qualification);
+            String result = ob.toString();
+            System.out.println(result);
+        }
+    }
+        
+        
+
+
+}
