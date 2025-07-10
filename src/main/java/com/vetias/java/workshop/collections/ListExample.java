c782bb048471d405ffec9f99c8bdccb657961e3e
logesh000777r <logesh000777r@gmail.com>
2025-07-10 14:53:57 +0530

    list

---
+++ src/main/java/com/vetias/java/workshop/collections/ListExample.java
@@ -0,0 +1,28 @@
+package com.vetias.java.workshop.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListExample {
+    public static void main(String[] args){
+        List<String> names=  new ArrayList<>();
+        names.add("Mithun");
+        names.add("Sachin");
+        names.add("Abullaize");
+        names.add("Logesh");
+        names.add("locke");
+        System.out.println(names);
+        System.out.println(names.set(1,"SACHINBABU"));
+        System.out.println(names.add(2,"babu"));
+
+
+        for(int i=0;i<names.size();i++){
+            System.out.println(names.get(i));
+        }
+        System.out.println(names.contains("Mithun"));
+        System.out.println(names.reversed());
+
+    }
+
+}
