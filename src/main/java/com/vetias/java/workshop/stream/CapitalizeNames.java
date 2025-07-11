213a02f3fcb98013187b5373a6da915e73df505c
logesh000777r <logesh000777r@gmail.com>
2025-07-11 13:30:22 +0530

    NamecapticaliseS

---
+++ src/main/java/com/vetias/java/workshop/stream/CapitalizeNames.java
@@ -0,0 +1,21 @@
+package com.vetias.java.workshop.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CapitalizeNames {
+    public static void main(String[] args){
+        List<String> names=  new ArrayList<>();
+        names.add("Mithun");
+        names.add("Sachin");
+        names.add("Abullaize");
+        names.add("Logesh");
+        names.add("Mithun");
+        names.add("locke");
+        System.out.println(names);
+        names.stream().filter(name->name.startsWith("S")||name.startsWith("s"))
+        .distinct().map(String::toUpperCase).forEach(System.out::println);
+
+    }
+
+}
