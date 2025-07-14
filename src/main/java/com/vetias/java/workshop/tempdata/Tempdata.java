4a560837136239e9a7bcabd32bc917e617a63487
logesh000777r <logesh000777r@gmail.com>
2025-07-14 12:14:33 +0530

    befor noon mom

--- src/main/java/com/vetias/java/workshop/tempdata/Tempdata.java
+++ src/main/java/com/vetias/java/workshop/tempdata/Tempdata.java
@@ -1,21 +1,36 @@
 package com.vetias.java.workshop.tempdata;
 
+import java.time.LocalDate;
+
+import com.vetias.java.workshop.tempdata.beans.Organization;
+
 public class Tempdata {
     public static void main(String[] args){
-    String setname = "VET Thindal";
-    double alatitude = 10.123456;
-    double alongitude = 78.123456;
-    String setaddress = "Thindal,Erode,TamilNadu,India";
-    String setpostalCode = "638012";
-    String setcontact = "9843170755";
+    // String setname = "VET Thindal";
+    // double alatitude = 10.123456;
+    // double alongitude = 78.123456;
+    // String setaddress = "Thindal,Erode,TamilNadu,India";
+    // String setpostalCode = "638012";
+    // String setcontact = "9843170755";
 
-    System.out.println("Name:" + setname);
-    System.out.println("Latitude:" + alatitude);
-    System.out.println("Longitude:" + alongitude);
-    System.out.println("Address:" + setaddress);
-    System.out.println("PostalCode:" + setpostalCode);
-    System.out.println("Contact:" + setcontact);
+    // System.out.println("Name:" + setname);
+    // System.out.println("Latitude:" + alatitude);
+    // System.out.println("Longitude:" + alongitude);
+    // System.out.println("Address:" + setaddress);
+    // System.out.println("PostalCode:" + setpostalCode);
+    // System.out.println("Contact:" + setcontact);
+        Organization vet= new Organization("Vellalar Educational Trust","Thindal,Erode","www.vet.com","contactvet.com",
+        "9843170755",123345678,LocalDate.of(1960,04,23));
+        System.out.println(vet);
+        System.out.println(vet.name());
+        System.out.println(vet.completeAddress());
+        System.out.println(vet.website());
+        System.out.println(vet.email());
+        System.out.println(vet.contactNumber());
+        System.out.println(vet.registration());
+        System.out.println(vet.registrationDate());
 
-}
+        
+    }
 
 }
---
+++ src/main/java/com/vetias/java/workshop/tempdata/beans/Organization.java
@@ -0,0 +1,9 @@
+package com.vetias.java.workshop.tempdata.beans;
+
+import java.time.LocalDate;
+
+public record Organization(String name,String completeAddress,String website,
+String email,String contactNumber,long registration,LocalDate registrationDate) {
+
+
+}
