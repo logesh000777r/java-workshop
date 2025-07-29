04fb5369d57ebde9360f89d5eb8a7dd77359a11d
logesh000777r <logesh000777r@gmail.com>
2025-07-29 22:23:32 +0530

    Day1 program2

---
+++ src/main/java/com/vetias/java/workshop/dailyprograms/NumberTriangle.java
@@ -0,0 +1,25 @@
+package com.vetias.java.workshop.dailyprograms;
+
+    import java.util.Scanner;
+
+public class NumberTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = scanner.nextInt();
+        scanner.close();
+
+        int current = 1;
+        for (int i = 1; i <= rows; i++) {
+            // inner loop: print i numbers on this row
+            for (int j = 1; j <= i; j++) {
+                System.out.print(current + " ");
+                current++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
